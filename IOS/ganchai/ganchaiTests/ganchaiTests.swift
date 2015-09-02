5419bf66a76204c1676907f9ebe7d615f6b32e77
Jay <673592063@qq.com>
2015-09-02 17:58:57 +0800

    init the ios switft version

---
+++ IOS/ganchai/ganchaiTests/ganchaiTests.swift
@@ -0,0 +1,36 @@
+//
+//  ganchaiTests.swift
+//  ganchaiTests
+//
+//  Created by 冯建 on 15/9/2.
+//  Copyright (c) 2015年 冯建. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ganchaiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
